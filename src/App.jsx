86cdec846e2554a26c2f2da1b1674eb4cf7b12d6
Mariana Cruz <marianacruz23@aragon.unam.mx>
2026-04-29 22:19:00 -0600

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = "https://paselista-back.onrender.com";
+const API_URL = "https://paselista-back.onrender.com/api";
 
 function App() {
   const [rol, setRol] = useState('alumno');
